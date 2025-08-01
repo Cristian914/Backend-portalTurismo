86076ece8ebda56744d05cf60ce46ad6dbef3e53
Cristian914 <cristianandrde914@gmail.com>
2025-08-01 14:38:14 -0300

    UPDATE

--- serve.js
+++ serve.js
@@ -10,7 +10,7 @@ const app = express();
  
 app.use(express.json());
 app.use(cors({                          
-  origin: 'http://localhost:5173',        
+  origin: 'https://frontend-portal-turismo-x12p.vercel.app/',        
   methods: ['GET','POST','PUT','DELETE'],
   credentials: true
 }));
